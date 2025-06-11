d3bdf6c3a5b2bd596b51863014b389219ca847d9
miloconner <mconner2525@gmail.com>
2025-06-11 17:17:52 -0500

    just testing stuff

--- app/src/main/java/perlin/App.java
+++ app/src/main/java/perlin/App.java
@@ -8,8 +8,6 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
-import util.Vec2;
-
 
 import java.util.Random;
 
@@ -63,49 +61,18 @@ public class App extends Application {
 
         int t = 0;
 
+        Perlin p = new Perlin(seed, block);
+        Perlin po = new Perlin(23424, block);
+        Perlin pi = new Perlin(seed, block/2);
+
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                int c1 = (int)Math.floor(i/block);
-                int c2 = c1 + 1;
-                int c3 = (int)Math.floor(j/block);
-                int c4 = c3 + 1;
-                double theta1 = getValueAt(c1,c3, seed);
-                Vec2 gradVec1 = new Vec2(Math.cos(theta1), Math.sin(theta1));
-                Vec2 offVec1 = new Vec2(i/block - c1, j/block - c3);
-                double theta2 = getValueAt(c2,c3, seed);
-                Vec2 gradVec2 = new Vec2(Math.cos(theta2), Math.sin(theta2));
-                Vec2 offVec2 = new Vec2(i/block - c2, j/block - c3);
-                double theta3 = getValueAt(c1,c4, seed);
-                Vec2 gradVec3 = new Vec2(Math.cos(theta3), Math.sin(theta3));
-                Vec2 offVec3 = new Vec2(i/block - c1, j/block - c4);
-                double theta4 = getValueAt(c2,c4, seed);
-                Vec2 gradVec4 = new Vec2(Math.cos(theta4), Math.sin(theta4));
-                Vec2 offVec4 = new Vec2(i/block - c2, j/block - c4);
-                //System.out.println(d1);
 
-                double sx = i/block - c1;
-                double sy = j/block - c3;
+                System.out.println((int)Math.floor((p.retrieve(i,j) + Math.sqrt(2)/2)*255/Math.sqrt(2)) + ", " + t + ", " + p.retrieve(i,j));
 
-                double f1 = smooth(sx);
-                double f2 = smooth(sy);
-
-                // double f1 = 6*Math.pow(sx, 5) - 15*Math.pow(sx, 4) + 10*Math.pow(sx, 3);
-                // double f2 = 6*Math.pow(sy, 5) - 15*Math.pow(sy, 4) + 10*Math.pow(sy, 3);
-
-                double d1 = gradVec1.dot(offVec1);
-                double d2 = gradVec2.dot(offVec2);
-                double d3 = gradVec3.dot(offVec3);
-                double d4 = gradVec4.dot(offVec4);
-                double i1 = d1 + f1*(d2 - d1);
-                double i2 = d3 + f1*(d4 - d3);
-                double finalValue = i1 + f2*(i2 - i1);
-                // finalValue = finalValue * 100;
-                // finalValue = Math.round(finalValue);
-                // finalValue = finalValue / 100;
-
-                System.out.println((int)Math.floor((finalValue + Math.sqrt(2)/2)*255/Math.sqrt(2)) + ", " + t + ", " + finalValue);
-
-                pixelWriter.setColor(i, j, Color.rgb((int)Math.floor((finalValue + Math.sqrt(2)/2)*255/Math.sqrt(2)), 0, 0));
+                pixelWriter.setColor(i, j, Color.rgb((int)Math.floor((p.retrieve(i, j) + Math.sqrt(2)/2)*255/Math.sqrt(2)),0, 0));
+                //                //(p.retrieve(i, j)+Math.pow(po.retrieve(i,j), 2)+pi.retrieve(i,j))
+                // (int)Math.floor((po.retrieve(i, j) + Math.sqrt(2)/2)*255/Math.sqrt(2)), (int)Math.floor((pi.retrieve(i, j) + Math.sqrt(2)/2)*255/Math.sqrt(2)))
                 t++;
             }
         }
